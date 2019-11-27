5a2fa54a25847d3e13ab33c04ab1a2282585e364
Remy Nijsten <99012252@mydavinci.nl>
2019-11-27 14:17:35 +0100

    Added Mad Libs

---
+++ paniek.php
@@ -0,0 +1,24 @@
+<html>
+	<head>
+		<title>Mad Libs</title>
+		<link rel="stylesheet" href="css/style.css">
+	</head>
+	<body>
+		<nav>
+			<ul>
+				<li>Paniek</li>
+				<li>Onkunde</li>
+			</ul>
+		</nav>
+		<div class="container">
+			<h1>Er heerst paniek...</h1><br>
+			<form action="paniek.php" method="post">
+				<p class="question">Welk dier zou je nooit als huisdier willen hebben?</p>
+				<input type="text" name="huisdier" class="answer"><br>
+			</form>
+		</div>
+
+
+		<footer>© Remy Nijsten - 2019</footer>
+	</body>
+</html>
